384243933c1f6270da30c3d9f6af373e0aeb9b29
Maifee Ul Asad <maifeeulasad@gmail.com>
2025-01-04 16:05:15 +0600

    [cleanup]: removed files which won't be used;

--- src/layout/CustomLayout.tsx
+++ src/layout/CustomLayout.tsx
@@ -15,8 +15,6 @@ const CustomHeader = () => {
       onBack={() => navigate(-1)}
       extra={
         <>
-          <Link to="/page2">Page 2</Link>
-          <Link to="/page3">Page 3</Link>
         </>
       }
     />
